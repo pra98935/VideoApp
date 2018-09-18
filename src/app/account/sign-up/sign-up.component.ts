95ad18449587103237f0d282da7f342d4b349c1c
User1 <arjun@eamandloi@eworkplaceapps.com>
2018-09-18 19:44:55 +0530

    validation

--- src/app/account/sign-up/sign-up.component.ts
+++ src/app/account/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { accountModel } from '../../model/account';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SIgnUpComponent implements OnInit {
 
+  acntModel:accountModel = new accountModel();
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  public submit(){
+    console.log(this.acntModel);
   }
 
 }
